f8e950bea3f3018d2a5c0c19853b534538252b98
DaveWexler <dwexler@gmail.com>
2016-07-25 16:33:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+
+  veto() {
+    return "NO!";
+  }
+  passBill() {
+    return "You can do that!";
+  }
+  doCharity() {
+    return "I like the help people.";
+  }
+  conductPressInterview() {
+    return "I am proud to be an American."
+  }
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}. I represent the ${this.politicalParty}s, and was in office ${this.yearsInOffice}.`
+  }
+}
+
+georgeWashington = new President("George Washington", "Democrat", 8, "Virginia");
+abrahamLincoln = new President("Abraham Lincoln", "Republican", 4, "Illinois");
+richardNixon = new President("Richard Nixon", "Republican", 4, "Prison");
+jimmyCarter = new President("Jimmy Carter", "Democrat", 4, "Iowa");
+
+
+// Then create the following presidents: George Washington, Abraham Lincoln, Richard Nixon, and Jimmy Carter.
